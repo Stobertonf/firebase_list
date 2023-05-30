120095ec248c3fbec650c9974df62db4318f646a
stobertonf <stobertonf@gmail.com>
2023-05-30 14:45:45 -0300

    feat(Home-Page): Adding Containers.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,6 +5,30 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.black,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
